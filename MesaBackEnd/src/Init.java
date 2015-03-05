4c4007d45b40438199f63de1a9a37141a331c602
hackjunky <hackjunky@ucmerced-10-31-21-40.ucmerced.edu>
2015-03-05 12:41:31 -0800

    Backend work started!

---
+++ MesaBackEnd/src/Init.java
@@ -0,0 +1,25 @@
+/**
+ * This class bootstraps the BackEnd. It should only contain configuration parameters, handle the loading from a configuration file, and pass parameters to the main
+ * class. See documentation for the API.
+ * 
+ * 
+ * @author hackjunky, jacrin
+ *
+ */
+
+public class Init {
+	
+	
+	public Init() {
+		
+	}
+	
+	
+	public void LoadConfigFile() {
+		
+	}
+	
+	public void ParseConfigFile() {
+		
+	}
+}
